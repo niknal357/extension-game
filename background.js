5f7f7bf6851f6b94ed89ae91a19c59542f0c0f36
niknal357 <46793548+niknal357@users.noreply.github.com>
2023-01-16 21:50:19 +0100

    ffff

--- background.js
+++ background.js
@@ -2,9 +2,9 @@ chrome.runtime.onMessage.addListener(function (request) {
     if (request.scheme == "dark") {
         chrome.browserAction.setIcon({
             path: {
-                128: "icons/icon_dark.png",
-                48: "icons/icon_dark.png",
-                16: "icons/icon_dark.png",
+                128: "icons/icon2.png",
+                48: "icons/icon2.png",
+                16: "icons/icon2.png",
             },
         });
     }
