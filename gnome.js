5a69f0ba58bb40b45aeb5fcbaf3484f28304dfd9
niknal357 <46793548+niknal357@users.noreply.github.com>
2023-01-16 21:34:14 +0100

    rrrrr

--- gnome.js
+++ gnome.js
@@ -1,54 +1,57 @@
-setTimeout(ready, 1000);
+setTimeout(ready, 100);
 
+function ready() {
+    let gnomeButton = document.getElementById("gnome-dex-button");
 
-function ready() { 
-    let gnomeButton = document.getElementById('gnome-dex-button')
+    gnomeButton.addEventListener("click", function () {
+        document
+            .getElementById("gnome-dex")
+            .classList.toggle("gnome-dex-hidden");
+        document
+            .getElementById("gnome-dex-button")
+            .classList.toggle("gnome-dex-button-toggled");
+    });
 
-    gnomeButton.addEventListener('click',
-        function(){
-            document.getElementById('gnome-dex').classList.toggle('gnome-dex-hidden');
-            document.getElementById('gnome-dex-button').classList.toggle('gnome-dex-button-toggled');
-        }
-    );
-
-    fetch('gnomes.txt')
-        .then(response => response.text())
-        .then(text => generateGnomeDex(text))
+    fetch("gnomes.txt")
+        .then((response) => response.text())
+        .then((text) => generateGnomeDex(text));
 }
 
-function generateGnomeDex(data){
+function generateGnomeDex(data) {
     // get rid of new lines in data
     data = data.replace(/(\r\n|\n|\r)/gm, "");
     console.log(data);
 
-    let lines = data.split(';');
+    let lines = data.split(";");
     let linesPerGnomeEntry = 3;
     let numGnomes = Math.floor(lines.length / linesPerGnomeEntry);
 
-    for (let i = 0; i < numGnomes; i++){
-        let gnome = document.createElement('div');
-        gnome.classList.add('gnome');
+    for (let i = 0; i < numGnomes; i++) {
+        let gnome = document.createElement("div");
+        gnome.classList.add("gnome");
 
-        let gnomeImage = document.createElement('img');
-        gnomeImage.classList.add('gnome-image');
-        gnomeImage.src = './gnomes/Level ' + (i + 1) + '.png';
+        let gnomeImage = document.createElement("img");
+        gnomeImage.classList.add("gnome-image");
+        gnomeImage.src = "./gnomes/Level " + (i + 1) + ".png";
         gnome.appendChild(gnomeImage);
 
-        let gnomeNameDescriptionContainer = document.createElement('div');
-        gnomeNameDescriptionContainer.classList.add('gnome-description-container');
+        let gnomeNameDescriptionContainer = document.createElement("div");
+        gnomeNameDescriptionContainer.classList.add(
+            "gnome-description-container"
+        );
         gnome.appendChild(gnomeNameDescriptionContainer);
 
-        let gnomeName = document.createElement('h2');
-        gnomeName.classList.add('gnome-name');
+        let gnomeName = document.createElement("h2");
+        gnomeName.classList.add("gnome-name");
         gnomeName.innerHTML = lines[i * linesPerGnomeEntry + 1];
         gnomeNameDescriptionContainer.appendChild(gnomeName);
 
-        let gnomeDescription = document.createElement('h3');
-        gnomeDescription.classList.add('gnome-description');
+        let gnomeDescription = document.createElement("h3");
+        gnomeDescription.classList.add("gnome-description");
         gnomeDescription.innerHTML = lines[i * linesPerGnomeEntry + 2];
         gnomeNameDescriptionContainer.appendChild(gnomeDescription);
 
         gnome.appendChild(gnomeNameDescriptionContainer);
-        document.getElementById('gnome-dex').appendChild(gnome);
+        document.getElementById("gnome-dex").appendChild(gnome);
     }
 }
